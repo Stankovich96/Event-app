670300daf25eb8587df55cef14101be73d940333
Stankovich96 <Okikestan@gmail.com>
2020-05-23 20:00:04 +0100

    My First React App

---
+++ build/precache-manifest.cd9bf8ff400754f5fd1606069d1696ab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adcd0b8c916fb414f38c9830b4e4bb5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0000b4f9397992b9aa0b",
+    "url": "/static/css/main.e016ecb0.chunk.css"
+  },
+  {
+    "revision": "ed2cf67ec26f2e6a5474",
+    "url": "/static/js/2.cc609172.chunk.js"
+  },
+  {
+    "revision": "06ad0cfa56fa8a2c9ccc7b2fff0ec8e0",
+    "url": "/static/js/2.cc609172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0000b4f9397992b9aa0b",
+    "url": "/static/js/main.0a1e7939.chunk.js"
+  },
+  {
+    "revision": "758b89daeb555eed73e5",
+    "url": "/static/js/runtime-main.1574e9c1.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
